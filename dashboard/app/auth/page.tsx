d2cd915ad2d07cb8c6ceb744913a7195d1d5df65
Felix Evers <git@felixevers.de>
2025-07-23 22:48:54 +0200

    fix: update redirect logic on auth indication

--- dashboard/app/auth/page.tsx
+++ dashboard/app/auth/page.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function SignInPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
   useEffect(() => {
-    signIn('keycloak');
-  });
+    if (status === 'loading') return;
+
+    if (session) {
+      router.replace('/');
+    } else {
+      signIn('keycloak');
+    }
+  }, [session, status, router]);
 
   return null;
 }
